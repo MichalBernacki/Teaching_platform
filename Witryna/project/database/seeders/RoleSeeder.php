ce612e970a8816caff03d41d5ac9e65a8a5f0185
Maciej Nikiel <mcwojs@student.agh.edu.pl>
2020-12-29 16:10:58 +0100

    Add seeder for roles

---
+++ Witryna/project/database/seeders/RoleSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'id' => 1,
+            'name' => 'None',
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 2,
+            'name' => 'Student',
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 3,
+            'name' => 'Lecturer',
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 4,
+            'name' => "Dean's office employee",
+        ]);
+    }
+}
