5dad801ca01fdfaa61b0201baade8ee3096c40bc
Maciej Nikiel <maciek@nikiel.org>
2020-12-30 11:24:16 +0100

    Add seeder for users with different access

--- Witryna/project/database/seeders/UserSeeder.php
+++ Witryna/project/database/seeders/UserSeeder.php
@@ -20,10 +20,32 @@ class UserSeeder extends Seeder
             'password' => bcrypt('secret'),
             'role_id' => 1
         ]);
+
         DB::table('users')->insert([
             'name' => 'Pablo Black',
             'email' => 'pb@g.com',
             'password' => bcrypt('linux'),
+            'role_id' => 3
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Dean Dean',
+            'email' => 'dean@dean.com',
+            'password' => bcrypt('dean'),
+            'role_id' => 4
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Student Student',
+            'email' => 'student@student.com',
+            'password' => bcrypt('student'),
+            'role_id' => 2
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Lecturer Lecturer',
+            'email' => 'lecturer@lecturer.com',
+            'password' => bcrypt('lecturer'),
             'role_id' => 3
         ]);
     }
