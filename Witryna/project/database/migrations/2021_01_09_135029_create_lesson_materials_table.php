8dfc7e4c9ccf691d839739a8e7708dab421a241d
Michał Bernacki <michal.bernacki121@gmail.com>
2021-01-09 15:04:59 +0100

    Added untracked files to previous commit

---
+++ Witryna/project/database/migrations/2021_01_09_135029_create_lesson_materials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lesson_materials', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('lesson_id');
+            $table->foreign('lesson_id')->references('id')->on('lessons');
+
+            $table->string('path');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lesson_materials');
+    }
+}
