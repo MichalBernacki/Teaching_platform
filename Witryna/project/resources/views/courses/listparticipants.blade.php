ff80892c6e8c13cb36cc946197a047fe9f6605c6
rriczku <kuba8rychlicki@gmail.com>
2021-01-09 22:32:43 +0100

    List of participants view

--- Witryna/project/resources/views/courses/listparticipants.blade.php
+++ Witryna/project/resources/views/courses/listparticipants.blade.php
@@ -5,21 +5,50 @@
         </h2>
     </x-slot>
 
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 bg-gradient-to-r from-green-400 to-blue-500">
-        <h1 class="font-mono">
-            {{$course->name}}
-        </h1>
-        <h1 class="font-mono">
-            List of participants:
-        </h1>
-        <div class="bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <table class="min-w-full divide-y divide-gray-200">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h2 class="text-lg font-semibold m-4">List of participants:</h2>
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            User ID
+                        </th>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Name
+                        </th>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Email
+                        </th>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Status
+                        </th>
+                        <th scope="col" class="relative px-6 py-3">
+                            <span class="sr-only">Change role</span>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
                 @foreach($course->users as $usr)
                     <tr>
-                        <td class="px-6 py-4 whitespace-nowrap">{{$usr->id}}</td>
-                        <td class="px-6 py-4 whitespace-nowrap"> {{$usr->name}}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{$usr->email}}</td>
-                        <td class="px-6 py-4 whitespace-nowrap"><strong>{{$usr->pivot->confirmed?'confirmed':'unconfirmed'}}</strong></td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-500">{{$usr->id}}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-500">{{$usr->name}}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-500">{{$usr->email}}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-500"><strong>{{$usr->pivot->confirmed?'confirmed':'unconfirmed'}}</strong></div>
+                        </td>
+
                         @if(!$usr->pivot->confirmed)
                             <td class="px-6 py-4 whitespace-nowrap">  <a href="{{route('courses.confirm',[$course->id,$usr->id])}}">Confirm</a></td>
                         @endif
@@ -27,6 +56,7 @@
                 @endforeach
             </table>
         </div>
+    </div>
     </div>
 
 </x-app-layout>
